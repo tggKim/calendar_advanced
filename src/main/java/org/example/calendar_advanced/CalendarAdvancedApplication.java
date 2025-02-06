09e378cf530692fe1affe020a013efc9596265c7
tgg <qazplmtfgcv123@gmail.com>
2025-02-06 10:57:09 +0900

    feat: 프로젝트 시작

---
+++ src/main/java/org/example/calendar_advanced/CalendarAdvancedApplication.java
@@ -0,0 +1,13 @@
+package org.example.calendar_advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarAdvancedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/calendar_advanced/CalendarAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.calendar_advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
