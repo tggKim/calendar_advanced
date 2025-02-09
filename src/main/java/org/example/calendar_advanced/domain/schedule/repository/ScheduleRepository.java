afe2bbf11bb0fb6289cc9f94357d5abaaff25035
tgg <qazplmtfgcv123@gmail.com>
2025-02-09 19:34:18 +0900

    feat: 일정 전체 조회 기능 작성
    - ScheduleController, ScheduleService, ScheduleRepsitory에 일정 전체 조회 기능 작성

--- src/main/java/org/example/calendar_advanced/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/org/example/calendar_advanced/domain/schedule/repository/ScheduleRepository.java
@@ -1,9 +1,16 @@
 package org.example.calendar_advanced.domain.schedule.repository;
 
+import org.example.calendar_advanced.domain.schedule.dto.ScheduleResponseDto;
 import org.example.calendar_advanced.domain.schedule.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    @Query("SELECT new org.example.calendar_advanced.domain.schedule.dto.ScheduleResponseDto(s.scheduleId, u.userId, u.username, s.title, s.todo, s.createdDate, s.updatedDate)  FROM Schedule s JOIN s.user u")
+    List<ScheduleResponseDto> getAllSchedules();
 }
