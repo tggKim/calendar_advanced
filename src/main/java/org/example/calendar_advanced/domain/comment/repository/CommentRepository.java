3d94060c1bc677a4ea4ca1d27cee093f72678220
tgg <qazplmtfgcv123@gmail.com>
2025-02-10 17:20:05 +0900

    fix: CommentRepository 단건 조회 기능 작성
    - getCommentById() 메서드 추가

--- src/main/java/org/example/calendar_advanced/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/calendar_advanced/domain/comment/repository/CommentRepository.java
@@ -4,9 +4,11 @@ import org.example.calendar_advanced.domain.comment.dto.CommentResponseDto;
 import org.example.calendar_advanced.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
@@ -14,4 +16,9 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query("SELECT new org.example.calendar_advanced.domain.comment.dto.CommentResponseDto(c.commentId, s.scheduleId, u.userId, u.username, c.content, c.createdDate, c.updatedDate)" +
             " FROM Comment c JOIN c.user u JOIN c.schedule s")
     List<CommentResponseDto> getAllComments();
+
+    @Query("SELECT new org.example.calendar_advanced.domain.comment.dto.CommentResponseDto(c.commentId, s.scheduleId, u.userId, u.username, c.content, c.createdDate, c.updatedDate)" +
+            " FROM Comment c JOIN c.user u JOIN c.schedule s" +
+            " WHERE c.commentId = :commentId")
+    Optional<CommentResponseDto> getCommentById(@Param("commentId") Long commentId);
 }
