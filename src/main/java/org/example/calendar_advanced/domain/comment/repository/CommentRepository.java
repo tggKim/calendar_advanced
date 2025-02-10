4081b0500bd889493ea3e1ce88d0e27bcdba7277
tgg <qazplmtfgcv123@gmail.com>
2025-02-10 16:24:21 +0900

    feat: CommentRepository 댓글 모두 조회기능 작성
    - getAllComments() 메서드 작성

--- src/main/java/org/example/calendar_advanced/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/calendar_advanced/domain/comment/repository/CommentRepository.java
@@ -1,9 +1,17 @@
 package org.example.calendar_advanced.domain.comment.repository;
 
+import org.example.calendar_advanced.domain.comment.dto.CommentResponseDto;
 import org.example.calendar_advanced.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query("SELECT new org.example.calendar_advanced.domain.comment.dto.CommentResponseDto(c.commentId, s.scheduleId, u.userId, u.username, c.content, c.createdDate, c.updatedDate)" +
+            " FROM Comment c JOIN c.user u JOIN c.schedule s")
+    List<CommentResponseDto> getAllComments();
 }
