9cb686cd5c04c0cf69a207678a1a6b6a2ec445e3
tgg <qazplmtfgcv123@gmail.com>
2025-02-10 14:47:25 +0900

    feat: CommentResponseDto 작성
    - CommentResponseDto 작성

---
+++ src/main/java/org/example/calendar_advanced/domain/comment/dto/CommentResponseDto.java
@@ -0,0 +1,29 @@
+package org.example.calendar_advanced.domain.comment.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class CommentResponseDto {
+    private Long commentId;
+    private Long scheduleId;
+    private Long userId;
+    private String content;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updatedTime;
+
+    @Builder
+    public CommentResponseDto(Long commentId, Long scheduleId, Long userId, String content, LocalDateTime createdTime, LocalDateTime updatedTime){
+        this.commentId = commentId;
+        this.scheduleId = scheduleId;
+        this.userId = userId;
+        this.content = content;
+        this.createdTime = createdTime;
+        this.updatedTime = updatedTime;
+    }
+}
